bbd5a0268208a1d3b71e489b6803f88cf378e535
18904306272 <932564901@qq.com>
2020-10-28 19:49:36 +0800

    1.0

--- src/components/HomeManager.vue
+++ src/components/HomeManager.vue
@@ -5,19 +5,68 @@
         经理
       </el-header>
       <el-container>
-        <el-aside class="aside">
+        <el-aside class="aside" width="100px">
           <!-- 侧边栏导航  -->
           aside
         </el-aside>
-        <el-main class="main">Main</el-main>
+        <el-main class="main">
+          <el-table
+            :data="tableData"
+            border
+            style="width: 100%">
+            <el-table-column
+              prop="empno"
+              label="员工编号"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="ename"
+              label="姓名"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="sex"
+              label="性别"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="tel"
+              label="電話"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="birthday"
+              label="生日"
+              width="180">
+            </el-table-column>
+          </el-table>
+        </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
-    name: "HomeManager"
+    name: "HomeManager",
+    data(){
+      return{
+        tableData: [],
+
+      }
+    },
+    methods:{
+      getInfo: function () { //获取经理个人信息
+        axios.get("http://localhost:8081/showInfo/1").then(res => {
+          this.tableData = res.data;
+        })
+      }
+    },
+    mounted() {
+      this.getInfo();
+    }
+
   };
 </script>
 <style scoped>
