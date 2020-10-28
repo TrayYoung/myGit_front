8d40c0990a41cf06e87cd8ae87d0623b1c6701e1
gy <gy@qq.com>
2020-10-28 15:28:41 +0800

    11

---
+++ src/components/HomeStudent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-container class="container">
+      <el-header class="header">
+        学生
+      </el-header>
+      <el-container>
+        <el-aside class="aside">
+          <!-- 侧边栏导航  -->
+          aside
+        </el-aside>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeStudent"
+  };
+</script>
+<style scoped>
+  .container {
+    height: 100vh;
+    font-size: 15px;
+  }
+  .header {
+    background: #212121;
+    color: #fff;
+  }
+  .aside {
+    background: #3a3a3a;
+    color: #fff;
+    /* height: 100%; */
+  }
+  .main {
+    /* height: 100%; */
+    color: #212121;
+  }</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld';
+import HomeStudent from "@/components/HomeStudent";
+import HomeTeacher from "@/components/HomeTeacher";
+import HomeEmp from "@/components/HomeEmp";
+import HomeManager from "@/components/HomeManager";
+import HomeAdmin from "@/components/HomeAdmin";
+
 
 Vue.use(Router)
 
@@ -10,6 +16,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path:'/homeStudent',
+      name:'HomeStudent',
+      component:HomeStudent
+    },{
+      path:'/homeTeacher',
+      name:'HomeTeacher',
+      component:HomeTeacher
+    },{
+      path:'/homeEmp',
+      name:'HomeEmp',
+      component:HomeEmp
+    },{
+      path:'/homeManager',
+      name:'HomeManager',
+      component:HomeManager
+    },{
+      path:'/homeAdmin',
+      name:'HomeAdmin',
+      component:HomeAdmin
     }
   ]
 })
