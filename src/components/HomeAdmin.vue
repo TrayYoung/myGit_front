5702a1bcebede92c07a7830a16178f847a26ca12
gy <gy@qq.com>
2020-10-29 10:46:21 +0800

    画出了侧边栏，实现了vuex的username显示

--- src/components/HomeAdmin.vue
+++ src/components/HomeAdmin.vue
@@ -16,7 +16,7 @@
           <el-col :span="8" class="rightsection">
 
             <div class="grid-content bg-purple-light">
-              <span class="el-dropdown-link userinfo-inner">欢迎您，{{$store.state.uid}}</span>
+              <span class="el-dropdown-link userinfo-inner">欢迎您，{{$store.state.username}}</span>
             </div>
           </el-col>
         </el-row>
@@ -24,7 +24,63 @@
       <el-container>
         <el-aside class="aside">
           <!-- 侧边栏导航  -->
-          aside
+
+          <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-menu-item index="1">
+              <template slot="title">
+                <i class="el-icon-document"></i>
+                <span>班级管理</span>
+              </template>
+           <!--   <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>-->
+            <!--  <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>-->
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-setting"></i>
+              <span slot="title">学生管理</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-setting"></i>
+              <span slot="title">教师管理</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">员工管理</span>
+            </el-menu-item>
+            <el-menu-item index="5">
+              <i class="el-icon-setting"></i>
+              <span slot="title">经理管理</span>
+            </el-menu-item>
+            <el-menu-item index="6">
+              <i class="el-icon-setting"></i>
+              <span slot="title">重置用户密码</span>
+            </el-menu-item>
+            <el-menu-item index="7">
+              <i class="el-icon-setting"></i>
+              <span slot="title">修改管理员密码</span>
+            </el-menu-item>
+            <el-menu-item index="8">
+              <i class="el-icon-setting"></i>
+              <span slot="title">退出登录</span>
+            </el-menu-item>
+          </el-menu>
         </el-aside>
         <el-main class="main">Main</el-main>
       </el-container>
