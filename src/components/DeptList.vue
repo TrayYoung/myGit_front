14ce8ffbce1791ac20ced66789bc65ebb2d4926a
18904306272 <932564901@qq.com>
2020-10-30 09:30:30 +0800

    1.1

--- src/components/DeptList.vue
+++ src/components/DeptList.vue
@@ -1,56 +1,73 @@
 <template>
-    <div>
-      <el-table
-        :data="tableData"
-        border
-        style="width: 100%">
-        <el-table-column
-          prop="empno"
-          label="员工编号"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="ename"
-          label="姓名"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="sex"
-          label="性别"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="birthday"
-          label="生日"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="籍贯"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="ismarry"
-          label="婚否"
-          width="100">
-        </el-table-column>
-        <el-table-column
-          prop="tel"
-          label="电话"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="idnum"
-          label="身份证号"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="dname"
-          label="所在部门"
-          width="100">
-        </el-table-column>
-      </el-table>
-    </div>
+  <div>
+    <!--查询功能-->
+    <el-input v-model="tableDataName" placeholder="请输入姓名" style="width:240px"></el-input>
+    <el-button type="primary" @click="doFilter">搜索</el-button>
+    <!--表格-->
+    <el-table
+      :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
+      @selection-change="handleSelectionChange"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="empno"
+        label="员工编号"
+        sortable
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="ename"
+        label="姓名"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        label="性别"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="birthday"
+        label="生日"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="籍贯"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="ismarry"
+        label="婚否"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="tel"
+        label="电话"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="idnum"
+        label="身份证号"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="dname"
+        label="所在部门"
+        sortable
+        width="120">
+      </el-table-column>
+    </el-table>
+    <!--分页器-->
+    <el-pagination align='center'
+                   @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange"
+                   :current-page="currentPage"
+                   :page-sizes="[5,10,20]"
+                   :page-size="pageSize"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="tableData.length">
+    </el-pagination>
+  </div>
 </template>
 
 <script>
@@ -61,6 +78,8 @@
     data() {
       return {
         tableData: [],
+        pagesize: 10,
+        currpage: 1
 
       }
     },
@@ -69,7 +88,18 @@
         axios.get("http://localhost:8080/getEmpByDeptno/1").then(res => {
           this.tableData = res.data;
         })
+      },
+
+      handleCurrentChange(cpage) {
+        this.currpage = cpage;
+      },
+      handleSizeChange(psize) {
+        this.pagesize = psize;
+      },
+      handleSelectionChange(val) {
+        console.log(val)
       }
+
     },
     mounted() {
       this.getEmpByDeptno();
