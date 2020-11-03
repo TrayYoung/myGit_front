9f398e6601fb141f6aa99cc478bfd170e4e84694
ko94360 <ko94360@qq.com>
2020-11-03 15:07:46 +0800

    个人信息超级究极完善

--- src/components/BaseInfo.vue
+++ src/components/BaseInfo.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <div style="width: 25%;height: 30%;
-              border: black;float: right;position: fixed;left: 80%">
-<!--      <img src="../assets/logo.png" alt="">-->
+    <div style="width: 20%;height: 30%;
+              border: black;float: right;position: absolute;left: 80%">
+      <el-upload
+        class="avatar-uploader"
+        action="http://localhost:8080/uploadFile"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload">
+        <img v-if="imageUrl" :src="imageUrl" class="avatar">
+        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
+      <span style="padding-left: 24%">1寸照片</span>
     </div>
       <div>
             <el-form ref="form" :model="form" label-width="100px">
@@ -63,6 +72,9 @@
     name: "BaseInfo",
     data(){
       return{
+        imageUrl:'',
+        uploadImgServer: 'http://localhost:8080/uploadFile',
+        imgLimit: 1, // 上传照片数
         form:{
           empno:'',
           ename:'',
@@ -76,24 +88,39 @@
           major:'',
           remarks:'',
           cname:''
+        },
+        rules:{
+
         }
       }
     },
-    methods:{
-      getStuMessage:function () {
+    methods: {
+      getStuMessage: function () {
         var empno = this.$store.state.uid;
-        axios.get("http://localhost:8080/getMessage/" + empno).then(res =>{
+        axios.get("http://localhost:8080/getMessage/" + empno).then(res => {
           this.form = res.data;
           var class_num = res.data.class_num;
-          this.$store.dispatch("setClassNo",class_num);
+          this.$store.dispatch("setClassNo", class_num);
         })
       },
-      onSubmit:function () {
-        alert(this.form.class_num)
-        this.$message("修改成功！")
+      handleAvatarSuccess(res, file) {
+        debugger
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
     },
-    //生命周期钩子
+
     mounted(){
       //编译后获取数据
       this.getStuMessage();
@@ -138,5 +165,30 @@
   .box-card {
     width: 100%;
     height: 95%;
+  }
+</style>
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
   }
 </style>
