7b42668753bf31db6087641a7ff1473af0aefc52
18904306272 <932564901@qq.com>
2020-11-09 20:15:46 +0800

    1.2

--- src/components/Regular_2.vue
+++ src/components/Regular_2.vue
@@ -53,7 +53,7 @@
         ></el-table-column>
         <el-table-column
           align="center"
-          width="120">
+          width="180">
           <template slot="header" slot-scope="scope">
             操作
           </template>
@@ -61,6 +61,11 @@
             <el-button
               size="mini"
               @click="testEmp(scope.row)">考核
+            </el-button>
+            <el-button
+              size="mini"
+              type="primary"
+              @click="showInfo(scope.row)">详细信息
             </el-button>
           </template>
         </el-table-column>
@@ -101,6 +106,267 @@
         <el-button type="primary" @click="submit">提交</el-button>
       </div>
     </el-dialog>
+
+    <el-dialog title="详细信息" :visible.sync="showInfoVisible" width="90%">
+
+
+      <el-table
+        :data="empInfo"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="empno"
+          label="员工编号">
+        </el-table-column>
+        <el-table-column
+          prop="ename"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="sex"
+          label="性别">
+        </el-table-column>
+        <el-table-column
+          prop="birthday"
+          label="生日">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="籍贯">
+        </el-table-column>
+        <el-table-column
+          prop="isMarry"
+          label="婚否">
+        </el-table-column>
+        <el-table-column
+          prop="tel"
+          label="电话">
+        </el-table-column>
+        <el-table-column
+          prop="idNum"
+          label="身份证号"
+          width="170px">
+        </el-table-column>
+        <el-table-column
+          prop="dname"
+          label="所在部门"
+          width="100">
+        </el-table-column>
+      </el-table>
+      <!--转正评价-->
+      <el-table
+        :data="regular"
+        border
+        style="width: 100%">
+        <el-table-column
+          label="转正工作评价"
+          align="center">
+          <el-table-column
+            prop="empno"
+            label="员工编号"
+            sortable>
+          </el-table-column>
+          <el-table-column
+            prop="ename"
+            label="姓名">
+          </el-table-column>
+          <el-table-column
+            label="员工部门"
+            prop="dname">
+          </el-table-column>
+
+          <el-table-column
+            label="员工职务"
+            prop="role">
+          </el-table-column>
+
+          <el-table-column
+            label="评价人"
+            prop="mname">
+          </el-table-column>
+
+          <el-table-column
+            label="评价分项">
+            <el-table-column
+              v-for="item in commentTableData"
+              :key="item.commentId"
+              :label="item.commentName"
+              :prop="item.commentName">
+            </el-table-column>
+          </el-table-column>
+          <el-table-column
+            label="整体评价分数"
+            prop="content_score"
+          ></el-table-column>
+          <el-table-column
+            label="评价（包括主要优点及缺陷）"
+            width="220px"
+            prop="content_text"
+          ></el-table-column>
+        </el-table-column>
+      </el-table>
+      <!--1年评价-->
+      <el-table
+        :data="regular1"
+        border
+        style="width: 100%">
+        <el-table-column
+          label="工作1年工作评价"
+          align="center">
+          <el-table-column
+            prop="empno"
+            label="员工编号"
+            sortable>
+          </el-table-column>
+          <el-table-column
+            prop="ename"
+            label="姓名">
+          </el-table-column>
+          <el-table-column
+            label="员工部门"
+            prop="dname">
+          </el-table-column>
+
+          <el-table-column
+            label="员工职务"
+            prop="role">
+          </el-table-column>
+
+          <el-table-column
+            label="评价人"
+            prop="mname">
+          </el-table-column>
+
+          <el-table-column
+            label="评价分项">
+            <el-table-column
+              v-for="item in commentTableData"
+              :key="item.commentId"
+              :label="item.commentName"
+              :prop="item.commentName">
+            </el-table-column>
+          </el-table-column>
+          <el-table-column
+            label="整体评价分数"
+            prop="content_score"
+          ></el-table-column>
+          <el-table-column
+            label="评价（包括主要优点及缺陷）"
+            width="220px"
+            prop="content_text"
+          ></el-table-column>
+        </el-table-column>
+      </el-table>
+      <!--2年评价-->
+      <el-table
+        :data="regular2"
+        border
+        style="width: 100%">
+        <el-table-column
+          label="工作2年工作评价"
+          align="center">
+          <el-table-column
+            prop="empno"
+            label="员工编号"
+            sortable>
+          </el-table-column>
+          <el-table-column
+            prop="ename"
+            label="姓名">
+          </el-table-column>
+          <el-table-column
+            label="员工部门"
+            prop="dname">
+          </el-table-column>
+
+          <el-table-column
+            label="员工职务"
+            prop="role">
+          </el-table-column>
+
+          <el-table-column
+            label="评价人"
+            prop="mname">
+          </el-table-column>
+
+          <el-table-column
+            label="评价分项">
+            <el-table-column
+              v-for="item in commentTableData"
+              :key="item.commentId"
+              :label="item.commentName"
+              :prop="item.commentName">
+            </el-table-column>
+          </el-table-column>
+          <el-table-column
+            label="整体评价分数"
+            prop="content_score"
+          ></el-table-column>
+          <el-table-column
+            label="评价（包括主要优点及缺陷）"
+            width="220px"
+            prop="content_text"
+          ></el-table-column>
+        </el-table-column>
+      </el-table>
+
+      <!--3年评价-->
+      <el-table
+        :data="regular3"
+        border
+        style="width: 100%">
+        <el-table-column
+          label="工作3年工作评价"
+          align="center">
+          <el-table-column
+            prop="empno"
+            label="员工编号"
+            sortable>
+          </el-table-column>
+          <el-table-column
+            prop="ename"
+            label="姓名">
+          </el-table-column>
+          <el-table-column
+            label="员工部门"
+            prop="dname">
+          </el-table-column>
+
+          <el-table-column
+            label="员工职务"
+            prop="role">
+          </el-table-column>
+
+          <el-table-column
+            label="评价人"
+            prop="mname">
+          </el-table-column>
+
+          <el-table-column
+            label="评价分项">
+            <el-table-column
+              v-for="item in commentTableData"
+              :key="item.commentId"
+              :label="item.commentName"
+              :prop="item.commentName">
+            </el-table-column>
+          </el-table-column>
+          <el-table-column
+            label="整体评价分数"
+            prop="content_score"
+          ></el-table-column>
+          <el-table-column
+            label="评价（包括主要优点及缺陷）"
+            width="220px"
+            prop="content_text"
+          ></el-table-column>
+        </el-table-column>
+      </el-table>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="showInfoVisible = false">取 消</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -120,7 +386,13 @@
           ]
         },
         dialogFormVisible: false,
+        showInfoVisible: false,
         tableData: [],
+        empInfo: [],
+        regular:[],
+        regular1:[],
+        regular2:[],
+        regular3:[],
         dialog: false,
         formLabelWidth: '100px',
         form: {
@@ -141,7 +413,7 @@
     },
     methods: {
       getRegular: function () {
-        axios.get("http://localhost:8080/getAllSumCommentCompany/" + this.$store.state.uid + "/regular2/" + this.form.year).then(res => {
+        axios.get("getAllSumCommentCompany/" + this.$store.state.uid + "/regular2/" + this.form.year).then(res => {
           this.tableData = res.data;
         })
 
@@ -184,6 +456,7 @@
         this.form.score3 = this.form.data[2];
         this.form.score4 = this.form.data[3];
         this.form.score5 = this.form.data[4];
+        // alert(this.form.data[0] + "" + this.form.data[1] + "");
         axios({
           method: 'post',
           url: '/commentRegular',
@@ -193,6 +466,24 @@
           this.getRegular();
           this.dialogFormVisible = false;
           this.$refs['form'].resetFields();
+        })
+      },
+      showInfo: function (row) {
+        this.showInfoVisible = true;
+        axios.get("getOne/" + row.empno).then(res => {
+          this.empInfo = res.data;
+        });
+        axios.get("getComment/" + row.empno + "/regular/0").then(res => {
+          this.regular = res.data;
+        });
+        axios.get("getComment/" + row.empno + "/regular1/1").then(res => {
+          this.regular1 = res.data;
+        })
+        axios.get("getComment/" + row.empno + "/regular2/2").then(res => {
+          this.regular2 = res.data;
+        })
+        axios.get("getComment/" + row.empno + "/regular3/3").then(res => {
+          this.regular3 = res.data;
         })
       }
 
